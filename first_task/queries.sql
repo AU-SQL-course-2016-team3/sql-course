68c5436601c6c0056c2725161695e15ae1c20ea8
equivalence1 <equivalence1@gmail.com>
2016-10-20 02:21:40 +0300

    style fixes

--- first_task/queries.sql
+++ first_task/queries.sql
@@ -6,14 +6,14 @@ WHERE E.medal IS NOT NULL
 GROUP BY C.name;
 
 -- 2. 
-SELECT M.name 
+SELECT M.name
 FROM (
   SELECT C.name, COUNT(E.medal = 'gold') AS gold_cnt
   FROM Country C LEFT JOIN Sportsman S ON C.id = S.country_id
   LEFT JOIN SportsmanEvents E ON S.id = E.sportsman_id
   GROUP BY C.id
 ) AS M
-WHERE M.gold_cnt = 0; 
+WHERE M.gold_cnt = 0;
 
 -- 3.
 CREATE VIEW L AS (
@@ -22,7 +22,7 @@ CREATE VIEW L AS (
   JOIN Country ON H.country_id = Country.id
 );
 
-SELECT L.address, L.name, L.head_name, L.phone, L.country_name 
+SELECT L.address, L.name, L.head_name, L.phone, L.country_name
 FROM L
 WHERE L.id in (SELECT L.id from L GROUP BY L.id HAVING count(*) > 1)
 ORDER BY L.address;
@@ -32,7 +32,7 @@ DROP VIEW L;
 -- 4. 
 SELECT C.name, SUM(case S.id is NULL when true then 0 else 1 end)
 FROM Country C LEFT JOIN Sportsman S ON S.country_id = C.id
-GROUP BY C.id; 
+GROUP BY C.id;
 
 -- 5.
 CREATE VIEW R AS (
@@ -43,7 +43,7 @@ CREATE VIEW R AS (
 
 SELECT *
 FROM R
-WHERE R.cnt IN 
+WHERE R.cnt IN
 ((SELECT MIN(cnt) FROM R) UNION ALL (SELECT MAX(cnt) FROM R));
 
 DROP VIEW R;
@@ -53,29 +53,29 @@ DROP VIEW R;
 CREATE VIEW Events AS (
   WITH EventNum AS (
     SELECT E.id, E.event_date, E.sport_id, E.construction_id, SUM(case S.id is NULL when true then 0 else 1 end) as cnt
-    FROM Event E LEFT JOIN SportsmanEvents SE ON SE.event_id = E.id 
+    FROM Event E LEFT JOIN SportsmanEvents SE ON SE.event_id = E.id
     LEFT JOIN Sportsman S ON S.id = SE.sportsman_id
     GROUP BY E.id
   )
-  SELECT C.id, C.address, C.name, S.name as sport, E.event_date, cnt  
+  SELECT C.id, C.address, C.name, S.name as sport, E.event_date, cnt
   FROM Construction C JOIN EventNum E ON E.construction_id = C.id
   LEFT JOIN Sport S ON S.id = E.sport_id
 );
 
 
-SELECT * 
+SELECT *
 FROM ((
   SELECT address, name, sport, event_date, cnt, 'max' as type
   FROM (
     SELECT id, address, name, sport, event_date, cnt, MAX(cnt) OVER (PARTITION BY id) as max
-    FROM Events 
+    FROM Events
   ) as S
   WHERE cnt = S.max
 ) UNION ALL (
   SELECT address, name, sport, event_date, cnt, 'min' as type
   FROM (
     SELECT id, address, name, sport, event_date, cnt, MIN(cnt) OVER (PARTITION BY id) as min
-    FROM Events 
+    FROM Events
   ) as S
   WHERE cnt = S.min
 )) as Q
@@ -91,21 +91,21 @@ GROUP BY C.id
 ORDER BY gold_cnt DESC, silver_cnt DESC, bronze_cnt DESC;
 
 -- 10.
-WITH Count AS ( 
-  SELECT C.name, count(*) as cnt
+WITH Count AS (
+  SELECT C.name, COUNT(*) as cnt
   FROM Sportsman S JOIN Country C ON S.country_id = C.id
   GROUP BY C.id
-), 
+),
 PopSport AS (
-  SELECT F.name, max(cnt) as cnt
+  SELECT F.name, MAX(cnt) as cnt
   FROM (
-    SELECT C.name, SS.sport_id, count(*) as cnt
+    SELECT C.name, SS.sport_id, COUNT(*) as cnt
     FROM Sportsman S JOIN Country C ON S.country_id = C.id
     JOIN SportsmanSports SS ON SS.sportsman_id = S.id
     GROUP BY C.name, SS.sport_id
   ) AS F
   GROUP BY F.name
-) 
+)
 SELECT PS.name
 FROM PopSport PS JOIN Count C ON PS.name = C.name
 WHERE (C.cnt > 2) AND (2 * PS.cnt >= C.cnt);
